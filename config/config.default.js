6b16c6cc71314c1aa35d3cff42c3b707cf9abb24
Ming <676416687@qq.com>
2018-04-12 09:09:50 +0800

    project start

---
+++ config/config.default.js
@@ -0,0 +1,19 @@
+'use strict';
+const path = require('path')
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + 'mmmmmmn';
+
+  // add your config here
+  config.middleware = [];
+
+   // 保证构建的静态资源文件能够被访问到
+  config.static = {
+    prefix: '/public/',
+    dir: path.join(appInfo.baseDir, 'public')
+  };
+
+  return config;
+};
