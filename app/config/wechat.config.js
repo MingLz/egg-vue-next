084a125974637dd3ee6db6a4e0a9e837cde8d30b
Ming <676416687@qq.com>
2018-05-16 14:25:20 +0800

    modify ignore

--- app/config/wechat.config.js
+++ app/config/wechat.config.js
@@ -3,10 +3,10 @@ module.exports = {
     wechatToken: "xxx", //第一次在微信控制台保存开发者配置信息时使用
     appId: "xxx",
     appSecret: "xxx",
-    card: true, //开启卡券支持，默认关闭
-    payment: true, //开启支付支持，默认关闭
+    card: false, //开启卡券支持，默认关闭
+    payment: false, //开启支付支持，默认关闭
     merchantId: '', //商户ID
-    paymentSandBox: true, //沙箱模式，验收用例
+    paymentSandBox: false, //沙箱模式，验收用例
     paymentKey: '', //必传，验签密钥，TIP:获取沙箱密钥也需要真实的密钥，所以即使在沙箱模式下，真实验签密钥也需要传入。
     //pfx 证书
     paymentCertificatePfx: fs.readFileSync(path.join(process.cwd(), 'cert/apiclient_cert.p12')),
